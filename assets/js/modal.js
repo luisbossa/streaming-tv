6c4bcecb36fadb646a128c90bd1e47050be3a180
luisbossa <luisbossa@gmail.com>
2026-03-25 23:47:18 -0600

    new images

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,19 +1,15 @@
-// Elementos
 const openModalBtn = document.getElementById("openModalBtn");
 const closeModalBtn = document.getElementById("closeModalBtn");
 const modal = document.getElementById("premiumModal");
 
-// Abrir modal
 openModalBtn.addEventListener("click", () => {
   modal.classList.add("show");
 });
 
-// Cerrar con botón X
 closeModalBtn.addEventListener("click", () => {
   modal.classList.remove("show");
 });
 
-// Cerrar al hacer clic fuera del contenido
 window.addEventListener("click", (e) => {
   if (e.target === modal) modal.classList.remove("show");
 });
